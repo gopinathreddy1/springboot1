e8c0025a6b6ea566284bbeaeaf0e4029b7a850ac
tcd09 <tcd09@DESKTOP-GR071O1>
2019-11-30 08:30:26 +0530

    controller modification

--- src/main/java/com/tclabs/dlmmember/controllers/LoyaltyProgramMemberContoller.java
+++ src/main/java/com/tclabs/dlmmember/controllers/LoyaltyProgramMemberContoller.java
@@ -19,6 +19,7 @@ public class LoyaltyProgramMemberContoller {
 	@PostMapping("/create")
 		public Mono<LoyaltyBalance> create(@RequestBody LoyaltyBalance loyaltyBalance) throws NullDataException{
 	Mono<LoyaltyBalance> loyaltyBalances= loyaltyMemberServices.create(loyaltyBalance);
+	system.out.println("hello");
 	if(loyaltyBalances!=null){
 		return loyaltyBalances;
 	}
