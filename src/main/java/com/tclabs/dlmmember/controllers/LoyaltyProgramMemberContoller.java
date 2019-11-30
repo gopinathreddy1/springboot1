6fd63b0f96ece047df2aa3958ced3cd4480dabb1
tcd09 <tcd09@DESKTOP-GR071O1>
2019-11-30 08:31:01 +0530

    controller modified

--- src/main/java/com/tclabs/dlmmember/controllers/LoyaltyProgramMemberContoller.java
+++ src/main/java/com/tclabs/dlmmember/controllers/LoyaltyProgramMemberContoller.java
@@ -20,6 +20,7 @@ public class LoyaltyProgramMemberContoller {
 		public Mono<LoyaltyBalance> create(@RequestBody LoyaltyBalance loyaltyBalance) throws NullDataException{
 	Mono<LoyaltyBalance> loyaltyBalances= loyaltyMemberServices.create(loyaltyBalance);
 	system.out.println("hello");
+	system.out.println("this is gopi");
 	if(loyaltyBalances!=null){
 		return loyaltyBalances;
 	}
